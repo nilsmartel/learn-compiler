d39f8c1b594356767f5e4a5e023c5629f339255f
Nils Martel <nils.martel@graphmasters.net>
2020-04-05 19:37:37 +0200

    Implement more utility functions

--- learn-compiler/src/parse/util.rs
+++ learn-compiler/src/parse/util.rs
@@ -1,4 +1,8 @@
-use nom::IResult;
+use nom::{
+    character::complete::char,
+    sequence::{delimited, preceded},
+    IResult,
+};
 
 pub fn skip_whitespace<'a, F, T>(f: F) -> impl Fn(&'a str) -> IResult<&'a str, T>
 where
@@ -8,8 +12,18 @@ where
     use nom::multi::many0;
     let skip = many0(one_of("\n\r\t "));
 
+pub fn delimited_curly<'a, T>(
+    f: impl Fn(&'a str) -> IResult<&'a str, T>,
+) -> impl Fn(&'a str) -> IResult<&'a str, T> {
+    delimited(char('{'), f, char('}'))
+}
+
     move |s: &str| {
         let (result, _) = skip(s)?;
         f(result)
     }
+pub fn delimited_paren<'a, T>(
+    f: impl Fn(&'a str) -> IResult<&'a str, T>,
+) -> impl Fn(&'a str) -> IResult<&'a str, T> {
+    delimited(char('('), f, char(')'))
 }
