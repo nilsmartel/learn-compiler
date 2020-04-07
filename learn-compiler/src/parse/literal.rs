f4f6d99490480a0efa02e9c6ec6f109694bf08c0
Nils Martel <nils.martel@graphmasters.net>
2020-04-07 16:14:46 +0200

    Implement Parsing of Literals

---
+++ learn-compiler/src/parse/literal.rs
@@ -0,0 +1,32 @@
+use crate::parse::*;
+use nom::IResult;
+
+#[derive(Clone, Debug)]
+pub struct Literal {
+    pub ident: Ident,
+    pub call_arguments: Option<Vec<Expression>>,
+}
+
+impl Parse for Literal {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        use nom::{
+            character::complete::char,
+            combinator::{map, opt},
+            multi::separated_list,
+            sequence::pair,
+        };
+        map(
+            pair(
+                Ident::parse,
+                opt(util::delimited_paren(separated_list(
+                    util::skip_whitespace(char(',')),
+                    Expression::parse_ws,
+                ))),
+            ),
+            |(ident, call_arguments)| Literal {
+                ident,
+                call_arguments,
+            },
+        )(input)
+    }
+}
